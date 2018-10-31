d0602ddff4f37cc0573eb54dd5477cc913c103c5
MetelchenkoO <MetelchenkoO@lz.ua>
2018-10-31 19:40:22 +0200

    Components  Databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'recipe-book-app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
